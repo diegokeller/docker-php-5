088e16f3c0e043ee318dd42aea71bd543333c76a
Diego Keller <keller.diego@gmail.com>
2021-02-08 23:05:31 -0300

    Update README.md, docker-compose.yml, and 4 more files...

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # docker-php-5
 Ambiente de desenvolvimento em PHP usando docker. 
+
+Para subir o ambiente execute:
+```
+docker-compose up
+```
+
+Se você fizer alguma alteração na configuração do php.ini, pare o container e execute o comando abaixo para que ele construa o container com a nova configuração.
+```
+docker-compose up --build app_web app_db
+```
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "app_db";
+$username = "app";
+$db = "app";
+$password = "segredo";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conectado com sucesso";
+} catch(PDOException $e) {
+  echo "Falha ao conectar: " . $e->getMessage();
+}
